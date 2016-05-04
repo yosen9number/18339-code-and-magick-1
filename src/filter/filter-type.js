4e6f31c036c2f39e67fb1dbcf6b8775d6deab8d6
yosen9number <korosu.o.korosu@gmail.com>
2016-05-04 20:09:24 +0300

    module9-task1

---
+++ src/filter/filter-type.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** @enum {number} */
+module.exports = {
+  'ALL': 'reviews-all',
+  'RECENT': 'reviews-recent',
+  'GOOD': 'reviews-good',
+  'BAD': 'reviews-bad',
+  'POPULAR': 'reviews-popular'
+};
