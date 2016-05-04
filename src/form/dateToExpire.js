84ea3bb709609d22eab1257054de80ce71d94db3
yosen9number <korosu.o.korosu@gmail.com>
2016-05-04 20:20:04 +0300

    1

---
+++ src/form/dateToExpire.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Катэ on 20.04.2016.
+ */
+
+'use strict';
+
+//Вычисояем кол-во дней для хранения Куки
+//Вычесляется как:
+//1) Определяем кол-во дней, прошедших с последнего дня рождения
+//2) Прибавляем к текущей дате
+function dateToExpire() {
+  var today = new Date();
+  var dateBD = new Date(today.getFullYear(), 3, 8);
+  var dateExp;
+
+  if (dateBD > today) {
+    dateBD.setFullYear(dateBD.getFullYear() - 1);
+    dateExp = +today - +dateBD;
+  } else {
+    dateExp = +today - +dateBD;
+  }
+
+  var dateEnd = +Date.now() + +dateExp;
+
+  return new Date(dateEnd).toUTCString();
+}
+
+module.exports = dateToExpire;
